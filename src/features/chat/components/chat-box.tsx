5f3655cdb56e94c2f6a2b0829c7620d987bbaf39
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-05 18:27:40 +0700

    feat: Add ChatBox and ChatSetting components

---
+++ src/features/chat/components/chat-box.tsx
@@ -0,0 +1,5 @@
+export interface ChatBoxProps {}
+
+export const ChatBox = (props: ChatBoxProps) => {
+  return <div className="h-full w-full rounded-md bg-card">ChatBox</div>;
+};
---
+++ src/features/chat/components/chat-setting.tsx
@@ -0,0 +1,28 @@
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
+import { Button } from '@/components/actions/button';
+import Link from 'next/link';
+import { Typography } from '@/components/data-display';
+import { useQueryParams } from '@/hooks/use-query-params';
+
+export interface ChatSettingProps {}
+
+export const ChatSetting = (props: ChatSettingProps) => {
+  const { pathname } = useQueryParams();
+  return (
+    <div className="h-full w-full rounded-md bg-card shadow-sm">
+      <div className="flex items-center gap-2 p-4">
+        <Link href={pathname}>
+          <Button.Icon
+            variant="ghost"
+            size="sm"
+            shape="circle"
+            className="-ml-2"
+          >
+            <ArrowLeftIcon />
+          </Button.Icon>
+        </Link>
+        <Typography variant="h3">Settings</Typography>
+      </div>
+    </div>
+  );
+};
