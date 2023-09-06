5687d26b0fb58da52b48d72db0ef916b746f54ac
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-07 00:18:09 +0700

    fix: Fix search input width issue

--- src/components/data-entry/search-input/search-input.tsx
+++ src/components/data-entry/search-input/search-input.tsx
@@ -21,7 +21,7 @@ export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
           <input
             ref={ref}
             type="text"
-            className="border-0 bg-inherit p-2 ring-0 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-transparent"
+            className="w-full border-0 bg-inherit p-2 ring-0 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-transparent"
             {...props}
           />
         </div>
@@ -51,23 +51,5 @@ const SearchButton = forwardRef<HTMLButtonElement, SearchButtonProps>(
     );
   },
 );
-
-//  ({}) => {
-//    return (
-//      <div className="flex h-10 w-10 items-center">
-//        <button className="dark:hover:bg-dark-600 flex h-10 w-10 cursor-pointer items-center justify-center hover:bg-slate-300">
-//          <MagnifyingGlassIcon
-//            className={cn(
-//              'h-6 w-6 transition-all duration-500',
-//              disabled
-//                ? 'text-normal'
-//                : 'text-primary-700 dark:text-primary-500',
-//            )}
-//          />
-//        </button>
-//        <div className="dark:bg-dark-400 h-[65%] w-[1px] bg-slate-400"></div>
-//      </div>
-//    );
-//  };
 
 SearchButton.displayName = 'SearchButton';
--- src/features/chat/components/inbox.tsx
+++ src/features/chat/components/inbox.tsx
@@ -1,18 +1,24 @@
 'use client';
 
-import { Cog6ToothIcon, SquaresPlusIcon } from '@heroicons/react/24/solid';
+import {
+  ArrowLeftIcon,
+  Cog6ToothIcon,
+  SquaresPlusIcon,
+} from '@heroicons/react/24/solid';
+import { Tabs, TabsList, TabsTrigger } from '@/components/navigation/tabs';
+import { useParams, useSearchParams } from 'next/navigation';
 
 import { Button } from '@/components/actions/button';
 import { ChatSetting } from './chat-setting';
-import { InboxFilter } from './inbox-filter';
 import { InboxItem } from '@/features/chat/components/inbox-item';
 import Link from 'next/link';
+import { SearchInput } from '@/components/data-entry';
 import { Typography } from '@/components/data-display';
 import { cn } from '@/lib/utils';
 import { roomsData } from '@/data/room';
 import { useQueryParams } from '@/hooks/use-query-params';
 import { useScrollDistanceFromTop } from '@/hooks/use-scroll-distance-from-top';
-import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 
 export interface InboxProps {}
 
@@ -60,16 +66,48 @@ const Header = () => {
 };
 
 const InboxDefaultTab = () => {
+  const [isSearch, setIsSearch] = useState(false);
   return (
     <div className="flex h-full flex-col">
-      <InboxFilter />
-      <InboxList />
+      <div className="flex w-full gap-1 px-4 py-2">
+        {isSearch && (
+          <Button.Icon
+            shape="circle"
+            variant="ghost"
+            onClick={() => setIsSearch(false)}
+            className="-ml-2"
+          >
+            <ArrowLeftIcon />
+          </Button.Icon>
+        )}
+        <div className="flex-1">
+          <SearchInput
+            onFocus={() => setIsSearch(true)}
+            btnDisabled
+            placeholder="Search talker"
+          />
+        </div>
+      </div>
+      <div className="relative flex flex-1 flex-col">
+        <Tabs defaultValue="all" className="mx-4 w-44 pb-2">
+          <TabsList className="grid grid-cols-2">
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="unread">Unread</TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <InboxList />
+        {isSearch && (
+          <div className="absolute left-0 top-0 min-h-full w-full bg-card"></div>
+        )}
+      </div>
     </div>
   );
 };
 
 const InboxList = () => {
   const { isScrolled, ref: scrollRef } = useScrollDistanceFromTop(1);
+  const params = useParams();
+  const id = params.id;
   return (
     <div
       ref={scrollRef}
@@ -79,7 +117,9 @@ const InboxList = () => {
       )}
     >
       {roomsData.map((room) => (
-        <InboxItem key={room._id} data={room} />
+        <Link key={room._id} href={`/talk/${room._id}`}>
+          <InboxItem key={room._id} data={room} isActive={id === room._id} />
+        </Link>
       ))}
     </div>
   );
