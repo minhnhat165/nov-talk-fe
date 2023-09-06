edc8ccd145da4ae6f3f36ed5e1c60969dd90eaa7
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-07 00:18:22 +0700

    feat: Add chat box component

--- src/features/chat/components/chat-box.tsx
+++ src/features/chat/components/chat-box.tsx
@@ -1,5 +1,124 @@
-export interface ChatBoxProps {}
+'use client';
+
+import {
+  CameraIcon,
+  DocumentIcon,
+  InformationCircleIcon,
+  MicrophoneIcon,
+  PaperAirplaneIcon,
+  PhoneIcon,
+  PhotoIcon,
+  PlusCircleIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/24/solid';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/data-display/popover';
+import { useMemo, useState } from 'react';
+
+import { Avatar } from '@/components/data-display/avatar';
+import { Button } from '@/components/actions/button';
+import { MessageBox } from '@/features/chat/components/message-box';
+import { Room } from '@/features/chat/types';
+import { Typography } from '@/components/data-display';
+import { generateRoomDisplay } from '../utils';
+
+export interface ChatBoxProps {
+  room: Room;
+}
 
 export const ChatBox = (props: ChatBoxProps) => {
-  return <div className="h-full w-full rounded-md bg-card">ChatBox</div>;
+  const [showSidebar, setShowSidebar] = useState(false);
+  return (
+    <div className="flex h-full">
+      <div className="flex h-full w-full flex-col overflow-hidden">
+        <Header room={props.room} />
+        <MessageBox />
+        <div className="flex h-20 w-full items-center justify-center bg-primary/5 px-10">
+          <ChatInput />
+        </div>
+      </div>
+      {showSidebar && (
+        <div className="w-[22.5rem] shrink-0 rounded-lg border-l bg-card"></div>
+      )}
+    </div>
+  );
+};
+
+const Header = ({ room: _room }: { room: Room }) => {
+  const room = useMemo(() => generateRoomDisplay(_room, '1'), [_room]);
+  return (
+    <div className="flex h-14 w-full items-center border-b px-4">
+      <div className="flex items-center gap-2">
+        <Avatar src={room.avatar!} alt={room.name!} />
+        <div>
+          <Typography variant="h4">{room.name}</Typography>
+        </div>
+      </div>
+      <div className="-mr-2 ml-auto">
+        <ActionBar />
+      </div>
+    </div>
+  );
+};
+
+const ActionBar = () => {
+  return (
+    <div>
+      <Button.Icon size="sm" variant="ghost" shape="circle">
+        <PhoneIcon />
+      </Button.Icon>
+      <Button.Icon size="sm" variant="ghost" shape="circle">
+        <VideoCameraIcon />
+      </Button.Icon>
+      <Button.Icon size="sm" variant="ghost" shape="circle">
+        <InformationCircleIcon />
+      </Button.Icon>
+    </div>
+  );
+};
+
+const ChatInput = () => {
+  return (
+    <div className=" flex w-full items-center gap-2">
+      <div className="flex h-14 flex-1 items-center gap-2 rounded-lg bg-card p-2 shadow-sm">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button.Icon size="md" variant="ghost" shape="circle">
+              <PlusCircleIcon />
+            </Button.Icon>
+          </PopoverTrigger>
+          <PopoverContent
+            className="w-fit p-2"
+            align="start"
+            sideOffset={16}
+            alignOffset={-8}
+          >
+            <Button.Icon size="sm" variant="ghost" shape="circle">
+              <CameraIcon />
+            </Button.Icon>
+            <Button.Icon size="sm" variant="ghost" shape="circle">
+              <PhotoIcon />
+            </Button.Icon>
+            <Button.Icon size="sm" variant="ghost" shape="circle">
+              <DocumentIcon />
+            </Button.Icon>
+            <Button.Icon size="sm" variant="ghost" shape="circle">
+              <MicrophoneIcon />
+            </Button.Icon>
+          </PopoverContent>
+        </Popover>
+        <input
+          type="text"
+          className="flex-1 bg-transparent outline-none"
+          placeholder="Type a message"
+        />
+      </div>
+      <Button.Icon size="lg" shape="circle" className="shrink-0">
+        <PaperAirplaneIcon />
+      </Button.Icon>
+    </div>
+  );
 };
--- src/features/chat/components/inbox-item.tsx
+++ src/features/chat/components/inbox-item.tsx
@@ -1,39 +1,137 @@
+import { Message, Room } from '@/features/chat/types';
+import { forwardRef, useMemo } from 'react';
+
 import { Avatar } from '@/components/data-display/avatar';
-import { Room } from '@/features/chat/types';
+import { Typography } from '@/components/data-display';
+import { User } from '@/features/user/types/user';
+import { cn } from '@/lib/utils';
 import { generateRoomDisplay } from '@/features/chat/utils';
-import { useMemo } from 'react';
+import { usersData } from '@/data/user';
 
 export interface InboxItemProps {
   data: Room;
+  isActive?: boolean;
 }
 
-export const InboxItem = ({ data: _data }: InboxItemProps) => {
-  const data = useMemo(() => generateRoomDisplay(_data, '1'), [_data]);
-  return (
-    <div className="flex cursor-pointer items-center justify-between rounded-md p-2 transition-all hover:bg-background">
-      <div className="flex w-full items-center gap-2">
-        <Avatar
-          alt="John Doe"
-          size="lg"
-          className="shrink-0"
-          src={data.avatar!}
-        />
-        <div className="w-full">
-          <div className="flex items-center justify-between">
-            <div className="max-w-full">
-              <span className="block">
+export const InboxItem = forwardRef<HTMLDivElement, InboxItemProps>(
+  ({ data: _data, isActive }, ref) => {
+    const currentUserId = usersData[0]._id;
+    const data = useMemo(
+      () => generateRoomDisplay(_data, currentUserId),
+      [_data, currentUserId],
+    );
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          'flex cursor-pointer items-center justify-between rounded-md p-2 transition-all',
+          isActive
+            ? 'bg-gradient-to-br from-primary/30 to-primary/5 shadow'
+            : 'bg-transparent hover:bg-background/75',
+        )}
+      >
+        <div className="flex w-full items-center gap-2">
+          <Avatar
+            alt="John Doe"
+            size="lg"
+            className="shrink-0"
+            src={data.avatar!}
+          />
+          <div className="w-full">
+            <div className="flex items-center justify-between">
+              <div className="max-w-full">
                 <span className="line-clamp-1 break-all font-semibold text-text/90">
                   {data.name}
                 </span>
-              </span>
+              </div>
+              <span className="ml-auto pl-2 text-sm text-text/50">12:00</span>
             </div>
-            <span className="ml-auto pl-2 text-sm text-gray-400">12:00</span>
-          </div>
-          <div className="text-sm text-gray-400">
-            {data.lastMessage?.content}
+            {data.lastMessage && (
+              <ItemSub
+                message={data.lastMessage}
+                participants={data.participants}
+                currentUserId={currentUserId}
+              />
+            )}
           </div>
         </div>
       </div>
+    );
+  },
+);
+
+InboxItem.displayName = 'InboxItem';
+
+const ItemSub = ({
+  message,
+  participants,
+  currentUserId,
+}: {
+  message: Message;
+  participants: User[];
+  currentUserId: User['_id'];
+}) => {
+  const readBy = message?.readBy || [];
+  const participantsById = useMemo(() => {
+    return participants.reduce(
+      (acc, cur) => {
+        acc[cur._id] = cur;
+        return acc;
+      },
+      {} as Record<string, User>,
+    );
+  }, [participants]);
+
+  const isRead = readBy.includes(currentUserId);
+
+  const preMessage = useMemo(() => {
+    if (message.sender._id === currentUserId) {
+      return 'You:';
+    }
+    if (participants.length > 2) {
+      return `${message.sender.name}:`;
+    }
+    return '';
+  }, [
+    message.sender._id,
+    message.sender.name,
+    currentUserId,
+    participants.length,
+  ]);
+
+  return (
+    <div className="flex items-center">
+      <Typography
+        className={cn(
+          'line-clamp-1 break-all text-sm',
+          isRead ? 'text-text/50' : 'font-bold  text-text/90',
+        )}
+      >
+        {preMessage} {message?.content}
+      </Typography>
+      {readBy.length > 0 && (
+        <div className="ml-auto flex items-center pl-2">
+          {readBy.map((userId) => {
+            if (
+              userId === currentUserId ||
+              !participantsById[userId] ||
+              message.sender._id === userId ||
+              message.sender._id !== currentUserId
+            ) {
+              return null;
+            }
+            return (
+              <div key={userId} className="shrink-0">
+                <Avatar
+                  alt={participantsById[userId].name}
+                  src={participantsById[userId].avatar}
+                  className="h-4 w-4"
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
