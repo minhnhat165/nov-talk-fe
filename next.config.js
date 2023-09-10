e48276162e1964a719f8c1f823796e4d44e99b35
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-10 20:08:26 +0700

    feat: Add new dependencies and configuration settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: [
+      'res.cloudinary.com',
+      'ui-avatars.com',
+      'lh3.googleusercontent.com',
+      'scontent-sea1-1.xx.fbcdn.net',
+      'platform-lookaside.fbsbx.com',
+    ],
   },
   experimental: {
     serverActions: true,
