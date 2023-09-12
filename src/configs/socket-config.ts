d80d46f82c7509236f17498b1d193bc93b26e7c5
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-12 23:12:22 +0700

    feat(socket-config): add new and delete events

--- src/configs/socket-config.ts
+++ src/configs/socket-config.ts
@@ -2,11 +2,13 @@ export const socketConfig = {
   events: {
     message: {
       new: 'message.new',
+      update: 'message.update',
     },
     room: {
       join: 'room.join',
       leave: 'room.leave',
       update: 'room.update',
+      delete: 'room.delete',
     },
   },
 };
