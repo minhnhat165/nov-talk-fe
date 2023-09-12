94ad5f4d8225c3ce920efde44ee9d806a0bc5ed5
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-13 00:27:19 +0700

    test(product): debug for deployment

--- src/app/(route-handler)/remote/auth/callback/route.ts
+++ src/app/(route-handler)/remote/auth/callback/route.ts
@@ -19,45 +19,49 @@ type TokenPayload = {
 export async function GET(req: NextRequest) {
   const accessToken = req.nextUrl.searchParams.get('access_token');
   const url = new URL('/api/auth/remote-login', appConfig.api.url);
-  // const res = await fetch(url, {
-  //   headers: {
-  //     Authorization: `Bearer ${accessToken}`,
-  //   },
-  // });
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
 
-  // if (!res.ok) {
-  //   return NextResponse.error();
-  // }
+  if (!res.ok) {
+    return NextResponse.json({
+      accessToken,
+      url: url.toString(),
+      message: 'Error: ' + res.statusText,
+    });
+  }
 
-  // const data: Response<{
-  //   tokens: Tokens;
-  //   user: User;
-  // }> = await res.json();
-  // const { accessToken: accessTokenValue, refreshToken: refreshTokenValue } =
-  //   data.data.tokens;
+  const data: Response<{
+    tokens: Tokens;
+    user: User;
+  }> = await res.json();
+  const { accessToken: accessTokenValue, refreshToken: refreshTokenValue } =
+    data.data.tokens;
 
-  // if (data) {
-  //   const accessTokenDecoded = jwtDecode<TokenPayload>(accessTokenValue);
-  //   const refreshTokenDecoded = jwtDecode<TokenPayload>(refreshTokenValue);
+  if (data) {
+    const accessTokenDecoded = jwtDecode<TokenPayload>(accessTokenValue);
+    const refreshTokenDecoded = jwtDecode<TokenPayload>(refreshTokenValue);
 
-  //   cookies().set({
-  //     name: appConfig.cookie.accessTokenName,
-  //     value: accessTokenValue,
-  //     path: '/',
-  //     expires: new Date(accessTokenDecoded.exp * 1000),
-  //     maxAge: accessTokenDecoded.exp - accessTokenDecoded.iat,
-  //   });
+    cookies().set({
+      name: appConfig.cookie.accessTokenName,
+      value: accessTokenValue,
+      path: '/',
+      expires: new Date(accessTokenDecoded.exp * 1000),
+      maxAge: accessTokenDecoded.exp - accessTokenDecoded.iat,
+    });
 
-  //   cookies().set({
-  //     name: appConfig.cookie.refreshTokenName,
-  //     value: refreshTokenValue,
-  //     path: '/',
-  //     expires: new Date(refreshTokenDecoded.exp * 1000),
-  //     maxAge: refreshTokenDecoded.exp - refreshTokenDecoded.iat,
-  //   });
+    cookies().set({
+      name: appConfig.cookie.refreshTokenName,
+      value: refreshTokenValue,
+      path: '/',
+      expires: new Date(refreshTokenDecoded.exp * 1000),
+      maxAge: refreshTokenDecoded.exp - refreshTokenDecoded.iat,
+    });
 
-  //   return redirect('/talk');
-  // }
+    redirect('/talk');
+  }
   return NextResponse.json({
     accessToken,
     url: url.toString(),
